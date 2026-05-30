f11a2133ad8eee7c962d9e9a92909322f710a2a4
Tair <gidrolaz.tr@gmail.com>
2026-05-30 20:36:04 +0700

    Админка для project

--- projects/admin.py
+++ projects/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Project
 
-# Register your models here.
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'status', 'created_at')
+    ordering = ('-created_at',)
