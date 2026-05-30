5b68798b4b43e821ca7b9baea982fa4abaab7108
Tair <gidrolaz.tr@gmail.com>
2026-05-30 19:40:00 +0700

    Представление модели user в админке

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
-# Register your models here.
+from .models import User, Skill
+
+
+class SkillInline(admin.TabularInline):
+    model = Skill
+    extra = 1
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    list_display = ('email', 'name', 'surname', 'is_staff', 'is_active')
+    ordering = ('-id',)
+    inlines = [SkillInline]
+
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Личные данные', {'fields': ('name', 'surname', 'avatar', 'phone', 'github_url', 'about')}),
+        ('Права', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('email', 'name', 'surname', 'password1', 'password2'),
+        }),
+    )
+
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'user')
