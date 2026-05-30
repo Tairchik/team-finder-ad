c42cbb420cafd23064bd13ea6aeef46847d33a88
Tair <gidrolaz.tr@gmail.com>
2026-05-30 20:48:27 +0700

    View для project

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.core.paginator import Paginator
+from django.http import JsonResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from django.views.decorators.http import require_POST
 
-# Create your views here.
+from .forms import ProjectForm
+from .models import Project
+
+
+def project_list_view(request):
+    projects = Project.objects.order_by('-created_at')
+    paginator = Paginator(projects, 9)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'projects/project_list.html', {
+        'page_obj': page_obj,
+        'query_prefix': '',
+    })
+
+
+def project_detail_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    return render(request, 'projects/project-details.html', {'project': project})
+
+
+@login_required
+def create_project_view(request):
+    form = ProjectForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        project = form.save(commit=False)
+        project.owner = request.user
+        project.save()
+        project.participants.add(request.user)
+        return redirect(f'/projects/{project.id}/')
+    return render(request, 'projects/create-project.html', {
+        'form': form,
+        'is_edit': False,
+    })
+
+
+@login_required
+def edit_project_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    if request.user != project.owner:
+        return redirect(f'/projects/{project_id}/')
+    form = ProjectForm(request.POST or None, instance=project)
+    if request.method == 'POST' and form.is_valid():
+        form.save()
+        return redirect(f'/projects/{project.id}/')
+    return render(request, 'projects/create-project.html', {
+        'form': form,
+        'is_edit': True,
+    })
+
+
+@login_required
+@require_POST
+def complete_project_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    if request.user != project.owner or project.status != 'open':
+        return JsonResponse({'status': 'error'}, status=403)
+    project.status = 'closed'
+    project.save()
+    return JsonResponse({'status': 'ok', 'project_status': 'closed'})
+
+
+@login_required
+@require_POST
+def toggle_participate_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    user = request.user
+    if user == project.owner:
+        return JsonResponse({'status': 'error'}, status=403)
+    if user in project.participants.all():
+        project.participants.remove(user)
+        participating = False
+    else:
+        project.participants.add(user)
+        participating = True
+    return JsonResponse({'status': 'ok', 'participating': participating})
+
+
+@login_required
+@require_POST
+def toggle_favorite_view(request, project_id):
+    project = get_object_or_404(Project, id=project_id)
+    user = request.user
+    if project in user.favorites.all():
+        user.favorites.remove(project)
+        is_favorite = False
+    else:
+        user.favorites.add(project)
+        is_favorite = True
+    return JsonResponse({'status': 'ok', 'is_favorite': is_favorite})
+
+
+@login_required
+def favorites_view(request):
+    projects = request.user.favorites.all().order_by('-created_at')
+    return render(request, 'projects/favorite_projects.html', {'projects': projects})
