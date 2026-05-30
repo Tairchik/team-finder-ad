439ab2deba2f810f5c5063327d597e0aa8c672ff
Tair <gidrolaz.tr@gmail.com>
2026-05-30 18:52:16 +0700

    Добавление базовых ссылок
    - Ссылки на приложения users, projects
    - Редирект (перенаправление) с пустой ссылки на /projects/list/

--- team_finder/urls.py
+++ team_finder/urls.py
@@ -3,7 +3,14 @@ from django.shortcuts import redirect
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('projects/', include('projects.urls')),
+    path('', RedirectView.as_view(url='/projects/list/', permanent=False)),  # редирект / → /projects/list/
 ]
+
+if settings.DEBUG:
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
